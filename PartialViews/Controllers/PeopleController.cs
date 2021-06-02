c64145e3d9862247931e3a0b0162a2ef1e01b20f
ChrisJohDev <chris@chrisjohannesson.com>
2021-06-02 14:48:16 +0200

    Modified for PartialView.

---
+++ PartialViews/Controllers/PeopleController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PartialViews.Models;
+using PartialViews.Models.ViewModels;
+
+namespace PartialViews.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public ViewResult Details()
+        {
+            PeopleDetailsViewModel peopleDetailsViewModel = new PeopleDetailsViewModel()
+            {
+                Title = "People Details List",
+                Header = "People Details"
+            };
+
+            return View(peopleDetailsViewModel);
+        }
+
+        public ViewResult Search(string searchString)
+        {
+            PeopleSearchViewModel peopleSearchViewModel = new PeopleSearchViewModel()
+            {
+                ListData = DataModel.Search(searchString),
+                Title = "People Search List",
+                Header = "People Search"
+            };
+
+            return View(peopleSearchViewModel);
+        }
+
+        public ViewResult Add(People person) { 
+
+            DataModel.Add(person);
+
+            PeopleDetailsViewModel peopleDetailsViewModel = new PeopleDetailsViewModel()
+            {
+                Title = "People Details List",
+                Header = "People Details"
+            };
+
+            return View("Details", peopleDetailsViewModel);
+        }
+
+        public ViewResult Delete(string Id)
+        {
+            int id = Convert.ToInt32(Id);
+
+            DataModel.Delete(id);
+
+            PeopleDetailsViewModel peopleDetailsViewModel = new PeopleDetailsViewModel()
+            {
+                Title = "People Details List",
+                Header = "People Details",
+            };
+            return View("Details", peopleDetailsViewModel);
+        }
+    }
+}
---
+++ PartialViews/Models/ViewModels/PeopleSearchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViews.Models.ViewModels
+{
+    public class PeopleSearchViewModel
+    {
+        public Persons ListData { get; set; }
+        public string Title { get; set; }
+        public string Header { get; set; }
+    }
+}
